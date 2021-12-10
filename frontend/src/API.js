2a6269c88167d59872ed366c4f0f41d712de1232
wabgam <wabgam@gmail.com>
2021-12-10 12:41:23 -0800

    second commit

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -5,7 +5,7 @@ var baseURL;
 if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
     baseURL = process.env.REACT_APP_API_BASE_URL;
 } else {
-    baseURL = 'http://127.0.0.1:8000';
+    baseURL = 'https://backend-gem.herokuapp.com/';
 }
 
 const api = axios.create({
